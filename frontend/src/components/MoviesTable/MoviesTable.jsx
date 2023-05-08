c4574bac1376ce41be0f80b3ee2706d6bb088292
WinterTee <27325188+wintertee@users.noreply.github.com>
2023-05-08 21:00:14 +0200

    refactor home page

--- frontend/src/components/MoviesTable/MoviesTable.jsx
+++ frontend/src/components/MoviesTable/MoviesTable.jsx
@@ -1,206 +1,38 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Button, Input, Menu, Modal, Pagination, Space } from 'antd';
+import { useState } from 'react';
+import { Button, Modal, Pagination, Space } from 'antd';
 import { Link } from 'react-router-dom';
 import { MovieCard } from '../MovieCard/MovieCard';
 import MovieInfo from '../MovieInfo/MovieInfo';
 
-// import './MoviesTable.css'
-const { Search } = Input;
-const API_KEY = '522d421671cf75c2cba341597d86403a';
-
-const useFetchMovies = (page, movieListType, setMovieList, isSearch) => {
-  useEffect(() => {
-    if (!isSearch) {
-      const api_url =
-        `https://api.themoviedb.org/3/movie/` +
-        movieListType +
-        `?api_key=` +
-        API_KEY +
-        `&page=` +
-        page;
-      axios
-        .get(api_url)
-        .then((response) => {
-          var movies = response.data.results;
-          console.log(movies);
-          movies.forEach((movie) => {
-            if (!('release_date' in movie)) {
-              movie.release_date = '1970-01-01';
-            }
-          });
-          setMovieList(movies);
-        })
-
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [page, movieListType, setMovieList, isSearch]);
-};
-
-const useSearchMovies = (page, query, setMovieList, isSearch) => {
-  useEffect(() => {
-    if (isSearch) {
-      const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=${page}&query=${query}`;
-      axios
-        .get(api_url)
-        .then((response) => {
-          var movies = response.data.results;
-          console.log(movies);
-          movies.forEach((movie) => {
-            if (!('release_date' in movie)) {
-              movie.release_date = '1970-01-01';
-            }
-          });
-          setMovieList(movies);
-        })
-
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [page, query, setMovieList, isSearch]);
-};
-
-const useAddMovieToDB = ({ movie }) => {
-  const [movieCreationError, setMovieCreationError] = useState(null);
-  const [movieCreationSuccess, setMovieCreationSuccess] = useState(null);
-  const displayCreationSuccessMessage = () => {
-    setMovieCreationSuccess('New movie created successfully');
-    setTimeout(() => {
-      setMovieCreationSuccess(null);
-    }, 3000);
-  };
-
-  const addMoviesToDB = (event) => {
-    // This avoid page reload
-    event.preventDefault();
-    axios
-      .post(`${import.meta.env.VITE_BACKDEND_URL}/movies/new`, {
-        name: movie.title,
-        date: movie.release_date,
-      })
-      .then(() => {
-        displayCreationSuccessMessage();
-      })
-      .catch((error) => {
-        setMovieCreationError('An error occured while creating new movie.');
-        console.error(error);
-      });
-  };
-
-  return { addMoviesToDB, movieCreationError, movieCreationSuccess };
-};
-
-// function AddMovieToDB(movies) {
-//   const { addMoviesToDB, movieCreationError, movieCreationSuccess } =
-//     useAddMovieToDB(movies);
-
-//   return (
-//     <div>
-//       <button
-//         className="add-movie-button"
-//         onClick={(event) => addMoviesToDB(event)}
-//       >
-//         Add all movie to database
-//       </button>
-//       {movieCreationSuccess !== null && (
-//         <div className="movie-creation-success">{movieCreationSuccess}</div>
-//       )}
-//       {movieCreationError !== null && (
-//         <div className="movie-creation-error">{movieCreationError}</div>
-//       )}
-//     </div>
-//   );
-// }
-
-function SelectMovieListTypeMenu({
-  movieListType,
-  setMovieListType,
-  setIsSearch,
-}) {
-  const items = [
-    {
-      label: 'Now Playing',
-      key: 'now_playing',
-    },
-    {
-      label: 'Top Rated',
-      key: 'top_rated',
-    },
-    {
-      label: 'Popular',
-      key: 'popular',
-    },
-    {
-      label: 'Upcoming',
-      key: 'upcoming',
-    },
-  ];
-
-  return (
-    <Menu
-      style={{ height: '46px' }}
-      onClick={(e) => {
-        setMovieListType(e.key);
-        setIsSearch(false);
-      }}
-      selectedKeys={[movieListType]}
-      mode="horizontal"
-      items={items}
-    />
-  );
-}
-
-function MoviesTable() {
-  const [movieList, setMovieList] = useState([]);
-  const [movieListType, setMovieListType] = useState('popular');
-  const [page, setPage] = useState(1);
+function MoviesTable({ movieList, page, setPage }) {
   const [modalMovieId, setModalMovieId] = useState(-1);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [query, setQuery] = useState('');
-  const [isSearch, setIsSearch] = useState(false);
-  useFetchMovies(page, movieListType, setMovieList, isSearch);
-  useSearchMovies(page, query, setMovieList, isSearch);
+
+  if (movieList.length === 0) {
+    return <div>loading</div>;
+  }
 
   return (
     <div>
-      <SelectMovieListTypeMenu
-        movieListType={movieListType}
-        setMovieListType={setMovieListType}
-        setIsSearch={setIsSearch}
-      />
-      <Search
-        size="large"
-        style={{ height: '46px' }}
-        placeholder="Search a movie"
-        onSearch={(value) => {
-          setQuery(value);
-          setIsSearch(true);
+      <Space
+        size={[8, 16]}
+        align={'center'}
+        wrap
+        style={{
+          justifyContent: 'center',
         }}
-      />
-      <div className="space-container">
-        <Space
-          size={[8, 16]}
-          align={'center'}
-          wrap
-          style={{
-            justifyContent: 'center',
-          }}
-        >
-          {movieList.map((movie) => (
-            <MovieCard
-              movie={movie}
-              onCardClick={() => {
-                setIsModalVisible(true);
-                setModalMovieId(movie.id);
-              }}
-              key={movie.id}
-            />
-          ))}
-        </Space>
-      </div>
+      >
+        {movieList.map((movie) => (
+          <MovieCard
+            movie={movie}
+            onCardClick={() => {
+              setIsModalVisible(true);
+              setModalMovieId(movie.id);
+            }}
+            key={movie.id}
+          />
+        ))}
+      </Space>
       <Modal
         open={isModalVisible}
         onCancel={() => setIsModalVisible(false)}
@@ -214,7 +46,7 @@ function MoviesTable() {
         <MovieInfo movieId={modalMovieId} />
       </Modal>
       <Pagination
-        defaultCurrent={1}
+        defaultCurrent={page}
         defaultPageSize={20}
         showSizeChanger={false}
         total={20 * 10}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,133 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button, Input, Menu, Modal, Pagination, Space } from 'antd';
+import { Link } from 'react-router-dom';
 import MoviesTable from '../../components/MoviesTable/MoviesTable';
-import './Home.css';
-function Home() {
-  return (
-    <div className="App">
-      <h1>Movie list</h1>
+import { MovieCard } from '../../components/MovieCard/MovieCard';
+import MovieInfo from '../../components/MovieInfo/MovieInfo';
 
-      <MoviesTable />
+const { Search } = Input;
+const API_KEY = '522d421671cf75c2cba341597d86403a';
+
+const useFetchMovies = (page, movieListType, setMovieList, isSearch) => {
+  useEffect(() => {
+    if (!isSearch) {
+      const api_url =
+        `https://api.themoviedb.org/3/movie/` +
+        movieListType +
+        `?api_key=` +
+        API_KEY +
+        `&page=` +
+        page;
+      axios
+        .get(api_url)
+        .then((response) => {
+          var movies = response.data.results;
+          console.log(movies);
+          movies.forEach((movie) => {
+            if (!('release_date' in movie)) {
+              movie.release_date = '1970-01-01';
+            }
+          });
+          setMovieList(movies);
+        })
+
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [page, movieListType, setMovieList, isSearch]);
+};
+
+const useSearchMovies = (page, query, setMovieList, isSearch) => {
+  useEffect(() => {
+    if (isSearch) {
+      const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=${page}&query=${query}`;
+      axios
+        .get(api_url)
+        .then((response) => {
+          var movies = response.data.results;
+          console.log(movies);
+          movies.forEach((movie) => {
+            if (!('release_date' in movie)) {
+              movie.release_date = '1970-01-01';
+            }
+          });
+          setMovieList(movies);
+        })
+
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [page, query, setMovieList, isSearch]);
+};
+
+function SelectMovieListTypeMenu({
+  movieListType,
+  setMovieListType,
+  setIsSearch,
+}) {
+  const items = [
+    {
+      label: 'Now Playing',
+      key: 'now_playing',
+    },
+    {
+      label: 'Top Rated',
+      key: 'top_rated',
+    },
+    {
+      label: 'Popular',
+      key: 'popular',
+    },
+    {
+      label: 'Upcoming',
+      key: 'upcoming',
+    },
+  ];
+
+  return (
+    <Menu
+      style={{ height: '46px' }}
+      onClick={(e) => {
+        setMovieListType(e.key);
+        setIsSearch(false);
+      }}
+      selectedKeys={[movieListType]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+}
+
+function Home() {
+  const [movieList, setMovieList] = useState([]);
+  const [movieListType, setMovieListType] = useState('popular');
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [isSearch, setIsSearch] = useState(false);
+  useFetchMovies(page, movieListType, setMovieList, isSearch);
+  useSearchMovies(page, query, setMovieList, isSearch);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Movie list</h1>
+      <SelectMovieListTypeMenu
+        movieListType={movieListType}
+        setMovieListType={setMovieListType}
+        setIsSearch={setIsSearch}
+      />
+      <Search
+        size="large"
+        style={{ height: '46px' }}
+        placeholder="Search a movie"
+        onSearch={(value) => {
+          setQuery(value);
+          setIsSearch(true);
+        }}
+      />
+      <MoviesTable movieList={movieList} page={page} setPage={setPage} />
     </div>
   );
 }
