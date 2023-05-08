78605305fda8306b0640034b1e26bbe3acec18a7
WinterTee <27325188+wintertee@users.noreply.github.com>
2023-05-08 15:43:26 +0200

    delete MovieInfoPage and Counter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import { Root } from './components/Root/Root';
-import Counter from './pages/Counter/Counter';
 import Users from './pages/Users/Users';
 import Films from './pages/Films/Films';
 import FilmDetail from './pages/FilmDetail/FilmDetail';
 import Login from './components/Login/Login';
-import MovieInfoPage from './pages/MovieInfoPage/MovieInfoPage';
 import UserSpace from './pages/UserSpace/UserSpace';
 
 function App() {
@@ -16,14 +14,12 @@ function App() {
     <Root>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="counter" element={<Counter />} />
         <Route path="users" element={<Users />} />
         <Route path="about" element={<About />} />
         <Route path="films" element={<Films />} />
         <Route path="filmpage/:mvid" element={<FilmDetail />} />
         <Route path="login" element={<Login />} />
         <Route path="userspace" element={<UserSpace />} />
-        <Route path="movie/:movieId" element={<MovieInfoPage />} />
       </Routes>
     </Root>
   );
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -12,10 +12,6 @@ const Header = () => {
         Home
       </Link>
       <div>|</div>
-      <Link className="Link" to="/counter">
-        Counter
-      </Link>
-      <div>|</div>
       <Link className="Link" to="/users">
         Users
       </Link>
