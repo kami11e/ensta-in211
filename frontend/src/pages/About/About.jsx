13269b3b65afa185af2f43e062961e340a45f3ed
WinterTee <27325188+wintertee@users.noreply.github.com>
2023-05-09 21:51:06 +0200

    add about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,7 +1,34 @@
-import './About.css';
-
+import React from 'react';
+import { Card } from 'antd';
+// import './About.css';
+const { Meta } = Card;
 function About() {
-  return <div className="about">about page</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Card
+        hoverable
+        style={{ width: '50%' }}
+        cover={
+          <img
+            alt="github-stats"
+            src="https://github-readme-stats.vercel.app/api/pin/?username=kami11e&repo=ensta-in211"
+          />
+        }
+        onClick={() =>
+          window.open('https://github.com/kami11e/ensta-in211', '_blank')
+        }
+      >
+        <Meta title="PROJET IN211" />
+      </Card>
+    </div>
+  );
 }
 
 export default About;
