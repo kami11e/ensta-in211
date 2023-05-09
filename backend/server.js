f1ad555f89274feb409460686b28b60832b28868
WinterTee <27325188+wintertee@users.noreply.github.com>
2023-05-09 23:07:56 +0000

    add webhoot (not tested yet)

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ import path from 'path';
 import { routeNotFoundJsonHandler } from './services/routeNotFoundJsonHandler.js';
 import { jsonErrorHandler } from './services/jsonErrorHandler.js';
 import { appDataSource } from './datasource.js';
-
+import crypto from 'crypto';
+import { exec } from 'child_process';
 // const apiRouter = express.Router();
 
 appDataSource
@@ -20,7 +21,13 @@ appDataSource
 
     app.use(logger('dev'));
     app.use(cors());
-    app.use(express.json());
+    app.use(
+      express.json({
+        verify: (req, res, buf) => {
+          req.rawBody = buf;
+        },
+      })
+    );
     app.use(express.urlencoded({ extended: false }));
 
     // Register routes
@@ -34,11 +41,42 @@ appDataSource
     // Register API router
     app.use('/api', apiRouter);
 
+    // Register Github Webhook
+    app.post('/webhook', (req, res) => {
+      const expectedSignature =
+        'sha256=' +
+        crypto
+          .createHmac('sha256', process.env.GITHUB_WEBHOOK_SECRET)
+          .update(req.rawBody)
+          .digest('hex');
+
+      const signature = req.headers['x-hub-signature-256'];
+
+      if (signature !== expectedSignature) {
+        return res.status(401).send(expectedSignature);
+      }
+
+      // Run your script
+      exec(
+        '/root/ensta-in211/run_webhook.sh >> /root/ensta-in211/webhook.log 2>&1',
+        (err, stdout, stderr) => {
+          if (err) {
+            // Handle error
+            console.error(err);
+            return;
+          }
+          console.log(stdout); //inutile...
+        }
+      );
+
+      res.json({ received: true });
+    });
+
     // Register frontend
-    const publicPath = new URL("./public", import.meta.url).pathname;
+    const publicPath = new URL('./public', import.meta.url).pathname;
     app.use(express.static(publicPath));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(publicPath, "index.html"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(publicPath, 'index.html'));
     });
 
     // Register 404 middleware and error handler
